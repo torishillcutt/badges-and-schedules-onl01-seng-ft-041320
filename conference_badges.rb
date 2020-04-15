7b9a8c7633bf9d9129ebce11107f6b9cb8d20d8b
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-15 17:50:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,9 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  badge_messages = []
+  array.each {|name| badge_messages << badge_maker(name)}
+  badge_messages
+end
